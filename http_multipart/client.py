94d021c13cd439c872eb0dbaa1c3d1daeff67c92
lvting <ting.lv@baishancloud.com>
2018-04-12 14:01:41 +0800

    modify client.py with format problem

--- http_multipart/client.py
+++ http_multipart/client.py
@@ -11,18 +11,15 @@ from random import Random
 from pykit import http
 from pykit import mime
 
-class HttpError(Exception):
+class MultipartError(Exception):
     pass
 
 
-class InvalidArgumentError(HttpError):
+class InvalidArgumentError(MultipartError):
     pass
 
 
-class SendFieldError(HttpError):
-    pass
-
-class NoneFieldError(HttpError):
+class SendFieldError(MultipartError):
     pass
 
 
@@ -30,10 +27,14 @@ class PostClient(object):
     def __init__(self, host, port, headers=None, timeout=60):
         self.host = host
         self.port = port
-        self.headers = copy.deepcopy(headers) or {}
         self.is_requested = False
         self.body_size = None
         self.has_content_length = True
+
+        if headers is None:
+            self.headers = {}
+        else:
+            self.headers = copy.deepcopy(headers)
 
         self.http_client = http.Client(self.host, self.port)
         self.boundary = uuid.uuid4().hex
@@ -49,7 +50,7 @@ class PostClient(object):
 
         return result
 
-    def send_fields(self, key_value_pair=None):
+    def send_fields(self, key_value_pair):
 
         if self.has_content_length is False and self.is_requested is True:
             raise SendFieldError('can not repeat to send field {x}'
@@ -62,15 +63,10 @@ class PostClient(object):
         for field_name, field in key_value_pair.items():
             self._send_one_field(field_name, field)
 
-        if key_value_pair is not None:
-            self.http_client.send_body('--'+self.boundary+'--')
-        else:
-            raise NoneFieldError('can not send None field {x}'
-                              .format(x=key_value_pair))
+        self.http_client.send_body('--'+self.boundary+'--')
 
     def _send_one_field(self, name, field):
-        f = copy.deepcopy(field)
-        value, headers = f['value'], f.get('headers', {})
+        value, headers = field['value'], field.get('headers', {})
 
         field_header = self._get_one_field_header(name, value, headers)
 
@@ -96,10 +92,11 @@ class PostClient(object):
         self.http_client.send_body('\r\n')
 
     def _get_one_field_header(self, name, value, headers):
-        field_header = ['--'+self.boundary]
+        headers = copy.deepcopy(headers)
+        field_header = ['--' + self.boundary]
 
         if isinstance(value, str):
-            field_header.append('Content-Dispostion: form-data;name='+name)
+            field_header.append('Content-Dispostion: form-data;name=' + name)
 
         elif isinstance(value, list):
             file_path, file_name = (value + [None])[:2]
@@ -108,7 +105,7 @@ class PostClient(object):
                 file_name = os.path.basename(file_path)
 
             field_header.append('Content-Dispostion: form-data;'
-                            +'name='+name+';filename='+file_name)
+                            + 'name='+name+ ';filename=' + file_name)
 
             if 'Content-Type' not in headers:
                 file_type = mime.get_by_filename(file_name)
@@ -121,12 +118,10 @@ class PostClient(object):
         return '\r\n'.join(field_header)
 
     def _get_one_field_size(self, name, field):
-        f = copy.deepcopy(field)
-        value, headers = f['value'], f.get('headers', {})
+        value, headers = field['value'], field.get('headers', {})
         field_size = 0
 
-        field_header = self._get_one_field_header(name,
-                        value, headers)
+        field_header = self._get_one_field_header(name, value, headers)
 
         field_size += len(field_header)
 
